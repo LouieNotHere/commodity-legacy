893d049421e0ec94ddae82e747de92a35ea7a60c
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-03-02 16:20:08 +0800

    Add Indonesian Localization

--- localization.ts
+++ localization.ts
@@ -11,18 +11,22 @@ const LOCALIZED_STRINGS: Record<string, Record<string, string>> = {
   "ribbonTooltip": {
     "en": "Commodity: Calculate Vault Value",
     "ja": "Commodity：ボールトの価値を計算",
+    "id": "Commodity: Kalkulasikan Nilai Vault"
   },
   "modalTitle": {
     "en": "Calculated Vault Value:",
     "ja": "計算されたボールトの価値:",
+    "id": "Nilai Kalkulasi Vault:"
   },
   "calculatingNotice": {
     "en": "Commodity (Legacy): Calculating the vault value...",
     "ja": "Commodity（レガシー）：ボールトの価値を計算中...",
+    "id": "Commodity (Warisan): Menghitung nilai vault..."
   },
   "calculatedTime": {
     "en": "Total CPU Time:",
     "ja": "合計CPU時間:",
+    "id": "Total Waktu CPU:"
   },
 };
 
